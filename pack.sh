12854b4e091fc4fa5600490691a087b69c46bb9a
Kai Lauterbach <klaute@web.de>
2011-04-16 11:25:55 +0200

    Added to build a firmware package.

---
+++ pack.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd ..
+
+DATE=`date -I | sed 'N;s/\n//'`
+
+7za -xr\!MikesLEDMatrix/Entwürfe a ~/Desktop/firmware_mikesledmatrix_$DATE.7z MikesLEDMatrix
+
+cd -
+
+cd ~/Desktop
+
+7za x firmware_mikesledmatrix_$DATE.7z
+
+tar cvzf firmware_mikesledmatrix_$DATE.tgz MikesLEDMatrix
+
+rm -rf MikesLEDMatrix
+
+cd -
+
