cace87476c7ee403e48a6ec84753be03319b116d
Kai Lauterbach <klaute@web.de>
2011-07-03 00:05:37 +0200

    string.h include added to fix the impliciit declaration warning from the gcc.

--- firmware/main.h
+++ firmware/main.h
@@ -17,6 +17,7 @@ You should have received a copy of the GNU General Public License along with thi
 #include <inttypes.h>
 #include <util/delay.h>
 #include <math.h>
+#include <string.h>
 
 #include <avr/interrupt.h>
 #include <avr/wdt.h>
